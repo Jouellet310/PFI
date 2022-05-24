26a4d2d8dc7ea6fb8995012faa476597f3b4e79a
Tristan <40375233+k86td@users.noreply.github.com>
2022-05-24 15:48:06 -0400

    Added script to upgrade database with the new tables.

---
+++ MySpace--DbUpgrade.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE Artists (
+	Id int PRIMARY KEY IDENTITY(1,1),
+	UserId int NOT NULL,
+	Name nvarchar(50) NOT NULL,
+	MainPhotoGUID nvarchar(100),
+	Description nvarchar(50),
+	Approved bit NOT NULL DEFAULT 0,
+	Visits int,
+	Likes int,
+
+	CONSTRAINT fk_artists_users FOREIGN KEY (userId) REFERENCES Users([Id]),
+);
+
+CREATE TABLE Videos (
+	Id int primary key identity(1,1),
+	ArtistId int not null,
+	YoutubeLink nvarchar(100) not null,
+	Creation datetime not null default GETDATE(),
+
+	CONSTRAINT uk_videos_youtubeLink UNIQUE(youtubeLink),
+	CONSTRAINT fk_videos_artists FOREIGN KEY (artistId) REFERENCES Artists(Id),
+);
+
+CREATE TABLE Messages (
+	Id int primary key identity(1,1),
+	ArtistId int not null,
+	UserId int not null,
+	Text nvarchar(50),
+	Creation datetime not null default GETDATE(),
+
+	CONSTRAINT fk_messages_users FOREIGN KEY (UserId) REFERENCES Users(Id),
+	CONSTRAINT fk_messages_artists FOREIGN KEY (ArtistId) REFERENCES Artists(Id),
+);
+
+CREATE TABLE FanLikes (
+	Id int primary key identity(1,1),
+	ArtistId int not null,
+	UserId int not null,
+	Creation datetime not null default GETDATE(),
+
+	CONSTRAINT fk_fanlikes_users FOREIGN KEY (UserId) REFERENCES Users(Id),
+	CONSTRAINT fk_fanlikes_artists FOREIGN KEY (ArtistId) REFERENCES Artists(Id),
+);
