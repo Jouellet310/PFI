3da195dfa49e0a568c9c5159b02f14d97a7a6d49
Tristan <40375233+k86td@users.noreply.github.com>
2022-05-29 21:59:56 -0400

    Able to Add and Remove youtube videos

--- Controllers/ArtistController.cs
+++ Controllers/ArtistController.cs
@@ -8,18 +8,18 @@ using System.Data.Entity;
 
 namespace MySpace.Controllers
 {
+    [ArtistAccess]
     public class ArtistController : Controller
     {
         MySpaceDBEntities DB = new MySpaceDBEntities();
-
-        [ArtistAccess]
+        
         public ActionResult EditProfile ()
         {
             Artist artist = DB.Artists.Where(a => a.UserId == OnlineUsers.CurrentUserId).First();
             return View(artist);
         }
 
-        [ArtistAccess, HttpPost]
+        [HttpPost]
         public ActionResult EditProfile(Artist artiste, string AvatarImageData)
         {
             Artist currentArtist = DB.Artists.Find(artiste.Id);
@@ -33,6 +33,64 @@ namespace MySpace.Controllers
             
             DB.SaveChanges();
             return RedirectToAction("Index", "MySpace");
+        }
+
+        [PropagateModelStateFromTempData]
+        public ActionResult Videos ()
+        {
+            return View(DB.Videos);
+        }
+
+        [RestoreModelStateFromTempData]
+        public ActionResult AddVideo()
+        {
+            return View();
+        }
+
+        [HttpPost, SetTempDataModelState]
+        public ActionResult AddVideo (Video video)
+        {
+            if (video.YoutubeLink == null || video.YoutubeLink == "")
+                ModelState.AddModelError("YoutubeLink", "Le lien youtube est obligatoire!");
+            if (video.Title == null || video.Title == "")
+                ModelState.AddModelError("Title", "Le titre est obligatoire!");
+
+            // parse link
+            const string youtubeVideoRequest = "https://www.youtube.com/watch?v=";
+            string ytId = "";
+            if (video.YoutubeLink.Contains(youtubeVideoRequest))
+            {
+                ytId = video.YoutubeLink.Replace(youtubeVideoRequest, "");
+                if (ytId.IndexOf("&") > -1)
+                    ytId = ytId.Substring(0, ytId.IndexOf("&"));
+            }
+            else
+                ModelState.AddModelError("YoutubeLink", "Le lien que vous avez entrer n'est pas valide");
+
+            if (ytId != "")
+            {
+                video.ArtistId = DB.Artists.Where(a => a.UserId == OnlineUsers.CurrentUserId).First().Id;
+                video.Creation = DateTime.Now;
+                video.YoutubeLink = ytId;
+            }
+            else
+                ModelState.AddModelError("YoutubeLink", "L'url n'a pas ete parser correctement");
+
+            if (!ModelState.IsValid)
+                return RedirectToAction("Videos");
+
+            DB.Videos.Add(video);
+            DB.SaveChanges();
+
+            return RedirectToAction("Videos");
+        }
+
+        public ActionResult RemoveVideo(int id)
+        {
+            Video video = DB.Videos.Find(id);
+            DB.Videos.Remove(video);
+            DB.SaveChanges();
+            return RedirectToAction("Videos");
         }
     }
 }
